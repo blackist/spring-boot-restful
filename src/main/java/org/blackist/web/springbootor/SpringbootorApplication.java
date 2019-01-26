52995d70d55a52fe122d4bcdc5a0f2c3a3a140f3
LiangLiang.Dong <liangl.dong@gmail.com>
2019-01-26 15:05:23 +0800

    [Repo] Project Init

---
+++ src/main/java/org/blackist/web/springbootor/SpringbootorApplication.java
@@ -0,0 +1,14 @@
+package org.blackist.web.springbootor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootorApplication.class, args);
+	}
+
+}
+
